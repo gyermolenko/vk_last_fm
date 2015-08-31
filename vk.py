13023b3c4d135dd63385530a5bb2cf0f095a750a
gyermolenko <gyermolenko@gmail.com>
2015-08-31 22:04:51 +0300

    Completed. v0.5

--- vk.py
+++ vk.py
@@ -4,6 +4,7 @@ import requests
 import json
 import os
 import datetime
+import last_fm as fm
 
 
 # APP_ID = '4945278'    # (client_id)
@@ -14,23 +15,13 @@ import datetime
 #   https://oauth.vk.com/blank.html#access_token=b970209a2b65a90c39b58f6c3e1cec344236d239c97e593cca2f02e546919990543a721832bc41b3f6a46&expires_in=86400&user_id=5110875
 #   3. update ACCESS_TOKEN variable below:
 
-ACCESS_TOKEN = 'b970209a2b65a90c39b58f6c3e1cec344236d239c97e593cca2f02e546919990543a721832bc41b3f6a46'  # Generated at (2015-08-03 13:59). Works for 24 hours ("expires_in=86400").
-
-track_name_01 = 'Hatebreed - I will be heard'  # this part will be modified to get list from txt or something
-# track_name_02 = 'Drowning Pool - Bodies'
-track_name_02 = u'mujuice - приключения'  # Test for cyrillic
-# track_name_01 = 'aarstarts'  # Test for no results
-# track_name_01 = 'random choice'  # Test for mismatch
-
-# list_of_tracks = [track_name_01]
-list_of_tracks = [track_name_01, track_name_02]  # Test for more than one
+ACCESS_TOKEN = '434d67b3fb5186bab7e0fc85b47da37d81144d44fa253f3243433b2360d388bbef33ed473f78441099aab'  # Generated at (2015-08-31 21-15). Works for 24 hours ("expires_in=86400").
 
 VK_METHOD_NAME = 'audio.search'
 RESULTS_QTY = '1'
 VK_API_VERSION = '5.35'  # 2015-08-02
 
-foldername = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
-os.mkdir(foldername)
+foldername = ''
 
 
 def get_vk_response(song_title):
@@ -45,53 +36,64 @@ def get_vk_response(song_title):
     return parsed_json
 
 
-def download_song(download_url, track_title):
-    filename = track_title + '.mp3'
+def download_song(download_url, song_title):
+    # global foldername
+    filename = song_title + '.mp3'
     with open(os.path.join(foldername, filename), 'w+') as f:
         f.write(requests.get(download_url).content)
 
 
+def process_json(parsed_json, song_title):
+    # Case of error in response. Err codes explained: http://vk.com/dev/errors
+    if 'error' in parsed_json:
+        # ex. 'User authorization failed: access_token has expired.'
+        print 'Error.', parsed_json['error']['error_msg']
+        return
+
+    # Case of 0 results found
+    if parsed_json['response']['count'] == 0:
+        print 'Track "{}" was not found.'.format(song_title)
+        return
+
+    # Case of title mismatch situation
+    artist = parsed_json['response']['items'][0]['artist']
+    title = parsed_json['response']['items'][0]['title']
+    song_title_from_response = ' - '.join((artist, title))
+    if song_title_from_response.lower() != song_title.lower():
+        print 'Track "{}", was not found. Closest match: "{}"'.format(song_title.encode('utf-8'), song_title_from_response.encode('utf-8'))
+        return
+
+    # Case when everything is OK
+    full_link = parsed_json['response']['items'][0]['url']
+    download_url = full_link.split('?')[0]  # ex. u'http://cs7-5v4.vk-cdn.net/p1/f2063e505df7b0.mp3'
+    download_song(download_url, song_title)
+    print 'Track "{}" processed.'.format(song_title.encode('utf-8'))
+
+
 def main():
-    for track_title in list_of_tracks:
-        parsed_json = get_vk_response(track_title)
+    global foldername
 
-        # Case of error in response ( error codes explained: http://vk.com/dev/errors )
-        if 'error' in parsed_json:
-            print 'Error.', parsed_json['error']['error_msg']  # ex. 'User authorization failed: access_token has expired.'
-            # TODO: add call to renew token
-            continue
+    last_fm_friends = fm.get_friends_list(fm.BASE_USERNAME)
+    friend = last_fm_friends[0]
 
-        # Case of 0 results found
-        if parsed_json['response']['count'] == 0:
-            print 'Track "{}" was not found.'.format(track_title)
-            continue
+    foldername = friend + '__' + datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+    os.mkdir(foldername)
 
-        # Case of title mismatch situation
-        artist = parsed_json['response']['items'][0]['artist']
-        title = parsed_json['response']['items'][0]['title']
-        song_title_from_response = ' - '.join((artist, title))
-        if song_title_from_response.lower() != track_title.lower():
-            print 'Track "{}", was not found. Closest match: "{}"'.format(track_title, song_title_from_response)
-            continue
+    his_loved_tracks = fm.get_loved_tracks(friend)
+    for track in his_loved_tracks[:35]:
+        if fm.get_artist_play_count_by_user(track[0], fm.BASE_USERNAME) < 4:
+            song_title = ' - '.join(track)
+            parsed_json = get_vk_response(song_title)
+            process_json(parsed_json, song_title)
 
-        # Case when everything is OK
-        full_link = parsed_json['response']['items'][0]['url']
-        download_url = full_link.split('?')[0]  # ex. u'http://cs7-5v4.vk-cdn.net/p1/f2063e505df7b0.mp3'
-        download_song(download_url, track_title)
 
 main()
 
 
 # TODO
-# v - if response contains error - return its text
-# v - analyse possible track title mismatch situation
-# v - "track not found" situation handling
-# v - put tracks into separate folder. Name it with current timestamp
-# v - try with 2-3 tracks finally
-# x - renew access_token automatically - impossible wo 3rd party tools
-# v - probably solved issue with cyrillic titles
+# v - last.fm part of application now works
+# v - combined with last.fm part.
+# v - folder is named with friend's name
 
-
-# - combine with track list from last.fm part
-
+# issues with some (not all) cylillyc. E.g. "Die Ärzte - Schrei nach Liebe"
 # GUI ?
